560378cf1d996ecbeba03467717bd3e38f682847
SangVUDINH <43957998+SangVUDINH@users.noreply.github.com>
2021-04-16 14:05:41 +0200

    add ldap authen

--- src/main/java/com/example/application/security/SecurityConfiguration.java
+++ src/main/java/com/example/application/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.application.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity 
@@ -19,8 +21,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String LOGIN_FAILURE_URL = "/login?error";
     private static final String LOGIN_URL = "/login";
     private static final String LOGOUT_SUCCESS_URL = "/login";
+    
+    
 
     @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth
+        .ldapAuthentication()
+          .userDnPatterns("uid={0},ou=people")
+          .groupSearchBase("ou=groups")
+          .contextSource()
+            .url("ldap://localhost:8389/dc=springframework,dc=org")
+            .and()
+          .passwordCompare()
+            .passwordEncoder(new BCryptPasswordEncoder())
+            .passwordAttribute("userPassword");
+    }
+
+	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()  
             .requestCache().requestCache(new CustomRequestCache()) 
